7d2a791674b5a488583070927297a5fb9334c991
sariyarawaekklang <sariyarawaekklang@gmail.com>
2022-01-07 15:28:50 -0600

    new files from npm and gitignore

--- app.js
+++ app.js
@@ -1,18 +1,23 @@
+const inquirer = require('inquirer');
+
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?'
+        }
+    ])
+    .then(answers => console.log(answers));
+/*
 const fs = require('fs');
-const generatePage = require('./src/page-template.js');
-
-const profileDataArgs = process.argv.slice(2);
-
-console.log(profileDataArgs);
-
-const [name, github] = profileDataArgs;
-
-console.log(name, github);
+const generatePage = require('./src/page-template');
 
 const pageHTML = generatePage(name, github);
 
 fs.writeFile('./index.html', pageHTML, err => {
-    if (err) throw (err);
+    if (err) throw err;
 
     console.log('Portfolio complete! Check out index.html to see the output!');
 });
+*/
