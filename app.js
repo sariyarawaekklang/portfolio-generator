33c8b92cecac746691eb3ff1be5ad41085a0bb33
sariyarawaekklang <sariyarawaekklang@gmail.com>
2022-01-08 00:13:01 -0600

    finished site

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-const fs = require('fs');
 const inquirer = require('inquirer');
 const generatePage = require('./src/page-template');
+const { writeFile, copyFile } = require('./utils/generate-site');
 
 const promptUser = () => {
     return inquirer.prompt([
@@ -122,16 +122,21 @@ Add a New Project
     });
 };
 
-
-
 promptUser()
     .then(promptProject)
     .then(portfolioData => {
-        const pageHTML = generatePage(portfolioData);
-        
-        fs.writeFile('./index.html', pageHTML, err => {
-            if (err) throw new Error(err);
-
-            console.log('Page created! Check out index.html in this directory to see it!');
-        });
+        return generatePage(portfolioData);
+    })
+    .then(pageHTML => {
+        return fs.writeFile(pageHTML);
+    })
+    .then(writeFileResponse => {
+        console.log(writeFileResponse);
+        return copyFile();
+    })
+    .then(copyFileReponse => {
+        console.log(copyFileReponse);
+    })
+    .catch(err => {
+        console.log(err);
     });
